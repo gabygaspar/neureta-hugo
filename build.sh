2e4030d029833e4d48e232406c9590424f717447
Javier Diaz <coderdiaz@gmail.com>
2020-10-13 11:44:11 -0500

    build: added script to build

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+if [[ ${VERCEL_GITHUB_COMMIT_REF} == 'master' ]]; then
+  echo ${VERCEL_GITHUB_COMMIT_REF};
+  echo "Runnning build using Production Environment";
+  hugo --gc --minify -b https://$BASE_URL
+else
+  echo ${VERCEL_GITHUB_COMMIT_REF};
+  echo "Runnning build using Staging Environment with Draft content enabled";
+  hugo --gc --minify -D --environment staging -b https://$VERCEL_URL
+fi
+
+# Runnning scripts transpilation
+npm run build
